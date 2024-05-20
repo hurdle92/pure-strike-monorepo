e14d04dc5912d2f860bf75a26a9c2c9c34f7b3ba
shpkc <tennisflow.contact@gmail.com>
2024-05-20 21:09:39 +0900

    feat : Link constants 추가

---
+++ apps/tiebreak-web/src/constants/link.ts
@@ -0,0 +1,5 @@
+export const APP_STORE_LINK =
+  "https://apps.apple.com/kr/app/%ED%83%80%EC%9D%B4%EB%B8%8C%EB%A0%88%EC%9D%B4%ED%81%AC/id6476483336";
+export const PLAY_STORE_LINK =
+  "https://play.google.com/store/apps/details?id=com.app.tiebreak&pcampaignid=web_share";
+export const DYNAMIC_LINK = "https://tiebreak.page.link/app";
