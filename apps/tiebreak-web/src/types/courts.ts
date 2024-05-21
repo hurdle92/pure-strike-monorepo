8bf60d361f017043349941d607d98468ee88e30a
shpkc <tennisflow.contact@gmail.com>
2024-05-21 23:10:56 +0900

    feat : court interface 추가

---
+++ apps/tiebreak-web/src/types/courts.ts
@@ -0,0 +1,12 @@
+export interface CourtItemInterface {
+  id: string;
+  koName: string;
+  thumbnail: string;
+  address: string;
+  isParking: boolean;
+  isOnlineReservation: boolean;
+  isRacketString: boolean;
+  isIndoor: boolean;
+  isShower: boolean;
+  isStore: boolean;
+}
