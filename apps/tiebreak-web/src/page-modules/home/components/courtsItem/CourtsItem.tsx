e5ff13ef76b015e84918e594b3f3d87ec93929bc
shpkc <tennisflow.contact@gmail.com>
2024-05-30 20:34:45 +0900

    feat : 링크 이동 임시 block

--- apps/tiebreak-web/src/page-modules/home/components/courtsItem/CourtsItem.tsx
+++ apps/tiebreak-web/src/page-modules/home/components/courtsItem/CourtsItem.tsx
@@ -14,30 +14,28 @@ export const CourtsItem = ({ item }: { item: CourtItemInterface }) => {
     isShower,
   } = item;
   return (
-    <Link href={`/courts/${id}`}>
-      <Stack padding={"16px 16px 0 16px"}>
-        <Image
-          src={thumbnail}
-          width={"100%"}
-          height={"200px"}
-          borderRadius={"8px"}
-          objectFit={"cover"}
-        />
-        <Spacer height={"8px"} />
-        <Text typo={"Text18Bold"}>{koName}</Text>
-        <Spacer height={"2px"} />
-        <Text typo={"Text14Regular"} color={"gray-500"}>
-          {address}
-        </Text>
-        <Spacer height={"4px"} />
-        <Flex gap={"8px"}>
-          {isOnlineReservation && <FeatureChip feature={"온라인 예약"} />}
-          {isIndoor && <FeatureChip feature={"실내코트"} />}
-          {isShower && <FeatureChip feature={"샤워시설"} />}
-        </Flex>
-        <Spacer height={"16px"} />
-        <Divider width={"100%"} height={"1px"} color={"border-gray"} />
-      </Stack>
-    </Link>
+    <Stack padding={"16px 16px 0 16px"}>
+      <Image
+        src={thumbnail}
+        width={"100%"}
+        height={"200px"}
+        borderRadius={"8px"}
+        objectFit={"cover"}
+      />
+      <Spacer height={"8px"} />
+      <Text typo={"Text18Bold"}>{koName}</Text>
+      <Spacer height={"2px"} />
+      <Text typo={"Text14Regular"} color={"gray-500"}>
+        {address}
+      </Text>
+      <Spacer height={"4px"} />
+      <Flex gap={"8px"}>
+        {isOnlineReservation && <FeatureChip feature={"온라인 예약"} />}
+        {isIndoor && <FeatureChip feature={"실내코트"} />}
+        {isShower && <FeatureChip feature={"샤워시설"} />}
+      </Flex>
+      <Spacer height={"16px"} />
+      <Divider width={"100%"} height={"1px"} color={"border-gray"} />
+    </Stack>
   );
 };
