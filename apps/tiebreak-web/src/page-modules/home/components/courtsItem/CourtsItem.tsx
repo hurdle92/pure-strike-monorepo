2bb6bd8ccc4a0a781047bbc65be15f7b5f724382
shpkc <tennisflow.contact@gmail.com>
2024-06-04 21:09:59 +0900

    feat : Courts Item img 컴포넌트 변경

--- apps/tiebreak-web/src/page-modules/home/components/courtsItem/CourtsItem.tsx
+++ apps/tiebreak-web/src/page-modules/home/components/courtsItem/CourtsItem.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import { Stack, Text, Image, Spacer, Divider, Flex } from "pure-strike-ui";
+import { Stack, Text, Spacer, Divider, Flex } from "pure-strike-ui";
+import { Img } from "src/components/img";
 import { CourtItemInterface } from "src/types/courts";
 import { FeatureChip } from "./components/FeatureChip";
 
@@ -14,30 +15,28 @@ export const CourtsItem = ({ item }: { item: CourtItemInterface }) => {
     isShower,
   } = item;
   return (
-    <Link href={`/courts/${id}`}>
-      <Stack padding={"16px 16px 0 16px"}>
-        <Image
-          src={thumbnail}
-          width={"100%"}
-          height={"200px"}
-          borderRadius={"8px"}
-          objectFit={"cover"}
-        />
-        <Spacer height={"8px"} />
-        <Text typo={"Text18Bold"}>{koName}</Text>
-        <Spacer height={"2px"} />
-        <Text typo={"Text14Regular"} color={"gray-500"}>
-          {address}
-        </Text>
-        <Spacer height={"4px"} />
-        <Flex gap={"8px"}>
-          {isOnlineReservation && <FeatureChip feature={"온라인 예약"} />}
-          {isIndoor && <FeatureChip feature={"실내코트"} />}
-          {isShower && <FeatureChip feature={"샤워시설"} />}
-        </Flex>
-        <Spacer height={"16px"} />
-        <Divider width={"100%"} height={"1px"} color={"border-gray"} />
-      </Stack>
-    </Link>
+    <Stack padding={"16px 16px 0 16px"}>
+      <Img
+        src={thumbnail}
+        isOrigin={true}
+        width={"100%"}
+        height={"200px"}
+        borderRadius={"8px"}
+      />
+      <Spacer height={"12px"} />
+      <Text typo={"Text18Bold"}>{koName}</Text>
+      <Spacer height={"2px"} />
+      <Text typo={"Text14Regular"} color={"gray-500"}>
+        {address}
+      </Text>
+      <Spacer height={"4px"} />
+      <Flex gap={"8px"}>
+        {isOnlineReservation && <FeatureChip feature={"온라인 예약"} />}
+        {isIndoor && <FeatureChip feature={"실내코트"} />}
+        {isShower && <FeatureChip feature={"샤워시설"} />}
+      </Flex>
+      <Spacer height={"16px"} />
+      <Divider width={"100%"} height={"1px"} color={"border-gray"} />
+    </Stack>
   );
 };
