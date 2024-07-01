969156ea745a1226e61184ddd9a3211d043aa3ae
shpkc <tennisflow.contact@gmail.com>
2024-07-01 21:17:25 +0900

    feat : courts list api 분리

--- apps/tiebreak-web/src/page-modules/courts/list/CourtsList.tsx
+++ apps/tiebreak-web/src/page-modules/courts/list/CourtsList.tsx
@@ -1,31 +1,13 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
 import { Flex, Stack } from "pure-strike-ui";
 import React from "react";
 import { InfiniteScrollTemplate } from "src/components/infinite/InfiniteScrollTemplate";
-import { PER_PAGE } from "src/constants/page";
-import { supabase } from "src/utils/apis/supabase/supabase";
 import { CourtItemInterface } from "src/types/courts";
 import { CourtsItem } from "./components/courtsItem/CourtsItem";
 import { CourtsListSkeleton } from "./components/skeleton/CourtsListSkeleton";
+import { useGetInfiniteCourtsList } from "src/apis/courts/queries";
 
 export const CourtsList = () => {
-  const { data, fetchNextPage, isFetching } = useInfiniteQuery({
-    queryKey: ["courts"],
-    queryFn: ({ pageParam }) => {
-      return supabase
-        .from("courts")
-        .select("*")
-        .eq("isUse", true)
-        .order("priority", { ascending: false })
-        .limit(30)
-        .range(pageParam * PER_PAGE, (pageParam + 1) * PER_PAGE - 1);
-    },
-    initialPageParam: 0,
-    getNextPageParam: (_, allPages) => {
-      const nextPage = allPages.length + 1;
-      return nextPage;
-    },
-  });
+  const { data, fetchNextPage, isFetching } = useGetInfiniteCourtsList();
 
   const currentCount = React.useMemo(() => {
     return data?.pages.flatMap((page) => page).length || 0;
@@ -46,7 +28,7 @@ export const CourtsList = () => {
         nextPage={() => fetchNextPage()}
         currentCount={currentCount}
         isLoading={isFetching}
-        fallbackUI={<CourtsListSkeleton />}
+        fallback={<CourtsListSkeleton />}
       >
         <Flex direction={"column"} gap={"16px"}>
           {listData?.map((item: CourtItemInterface) => {
