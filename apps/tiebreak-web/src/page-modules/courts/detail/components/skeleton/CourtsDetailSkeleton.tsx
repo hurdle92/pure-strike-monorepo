a77b5e2b0fe1d6430f5592cb307d0f67c9fc8d83
shpkc <tennisflow.contact@gmail.com>
2024-07-04 22:47:37 +0900

    feat : courts detail skeleton 수정

--- apps/tiebreak-web/src/page-modules/courts/detail/components/skeleton/CourtsDetailSkeleton.tsx
+++ apps/tiebreak-web/src/page-modules/courts/detail/components/skeleton/CourtsDetailSkeleton.tsx
@@ -5,10 +5,20 @@ export const CourtsDetailSkeleton = () => {
   return (
     <Stack width={"420px"}>
       <Skeleton width={"420px"} height={"280px"} />
-      <Stack padding={"16px"}>
+      <Stack padding={"16px 16px 32px 16px"}>
         <Skeleton width={"200px"} height={"30px"} />
         <Spacer height={"16px"} />
         <Skeleton width={"300px"} height={"20px"} />
+        <Spacer height={"16px"} />
+        <Skeleton width={"100px"} height={"20px"} />
+        <Spacer height={"48px"} />
+        <Skeleton width={"120px"} height={"30px"} />
+        <Spacer height={"16px"} />
+        <Skeleton width={"100%"} height={"60px"} />
+        <Spacer height={"80px"} />
+        <Skeleton width={"120px"} height={"30px"} />
+        <Spacer height={"16px"} />
+        <Skeleton width={"100%"} height={"60px"} />
       </Stack>
     </Stack>
   );
