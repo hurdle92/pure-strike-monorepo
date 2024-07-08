e111d72a8149610ebb2a7b89d5de75e1c6386a98
shpkc <tennisflow.contact@gmail.com>
2024-07-08 23:43:23 +0900

    feat : Aside search chip list 추가

---
+++ apps/tiebreak-web/src/constants/search.ts
@@ -0,0 +1,9 @@
+export const SEARCH_CHIPS_LIST = [
+  { name: "서초" },
+  { name: "MZB" },
+  { name: "올림픽공원" },
+  { name: "인천" },
+  { name: "리버사이드" },
+  { name: "랠리테니스" },
+  { name: "양재 시민의숲" },
+];
