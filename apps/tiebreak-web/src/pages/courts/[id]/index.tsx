7e7fa7073cc3c5dd17f2e8d699400165ffd67e1f
shpkc <tennisflow.contact@gmail.com>
2024-07-15 19:54:48 +0900

    feat : courts detail dehydrated로 전달

--- apps/tiebreak-web/src/pages/courts/[id]/index.tsx
+++ apps/tiebreak-web/src/pages/courts/[id]/index.tsx
@@ -1,4 +1,3 @@
-import dynamic from "next/dynamic";
 import React from "react";
 import CourtsDetail from "src/page-modules/courts/detail/CourtsDetail";
 import { CourtsDetailSkeleton } from "src/page-modules/courts/detail/components/skeleton/CourtsDetailSkeleton";
@@ -6,10 +5,8 @@ import { supabase } from "src/utils/supabase/supabase";
 import { seoMapper } from "src/utils/seo/seoMapper";
 import { CourtsDetailInterface } from "src/apis/courts/types";
 import { NextSeo } from "next-seo";
-
-const Layout = dynamic(() => import("src/components/layout/Layout"), {
-  ssr: false,
-});
+import Layout from "src/components/layout/Layout";
+import { dehydrate, QueryClient } from "@tanstack/react-query";
 
 const CourtsDetailPage = ({ id, seoData }) => {
   return (
@@ -26,13 +23,25 @@ export default CourtsDetailPage;
 
 export async function getServerSideProps(context) {
   const { id } = context.query;
-  const { data } = await supabase
-    .from("courts")
-    .select("*")
-    .eq("id", id)
-    .single();
 
-  const courtData: CourtsDetailInterface = data;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["courtsDetail", id],
+    queryFn: async () => {
+      const { data } = await supabase
+        .from("courts")
+        .select("*")
+        .eq("id", id)
+        .single();
+      return data;
+    },
+  });
+
+  const courtData = queryClient.getQueryData([
+    "courtsDetail",
+    id,
+  ]) as CourtsDetailInterface;
 
   return {
     props: {
@@ -41,6 +50,7 @@ export async function getServerSideProps(context) {
         title: courtData.koName,
         description: courtData.address,
       }),
+      dehydratedState: dehydrate(queryClient),
     },
   };
 }
