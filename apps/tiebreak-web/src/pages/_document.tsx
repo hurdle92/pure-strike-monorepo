a24f31f451c83f974c4bede0d438e002028fe86f
shpkc <tennisflow.contact@gmail.com>
2024-07-15 20:57:04 +0900

    Revert "feat : _document 수정"
    This reverts commit 66e8f7fe4bd198f4a07eaa93ba24fd40fef09cf0.

--- apps/tiebreak-web/src/pages/_document.tsx
+++ apps/tiebreak-web/src/pages/_document.tsx
@@ -1,11 +1,8 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
-import type { DocumentContext, DocumentInitialProps } from "next/document";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
@@ -19,7 +16,12 @@ export default class MyDocument extends Document {
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()],
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
       };
     } finally {
       sheet.seal();
