51a09e5b2c2e656378282dbdbaec9824cd585719
shpkc <tennisflow.contact@gmail.com>
2024-07-15 20:57:15 +0900

    Revert "doc : 불필요한 코드 삭제"
    This reverts commit ceb3d8e226cb8056008b9b455cd8d677d4e07320.

--- apps/tiebreak-web/src/pages/_app.tsx
+++ apps/tiebreak-web/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
-import { DefaultSeo } from "next-seo";
+import { DefaultSeo, NextSeo } from "next-seo";
 import {
   QueryClient,
   QueryClientProvider,
@@ -27,6 +27,8 @@ const queryClient = new QueryClient({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { seoData } = pageProps;
+
   return (
     <QueryClientProvider client={queryClient}>
       <NextTopLoader color={Palette["blue-primary"]} showSpinner={false} />
