177ec1421b52d6b9d1cc819098ff5e5b00a3fcb4
shpkc <tennisflow.contact@gmail.com>
2024-07-15 21:10:19 +0900

    Reapply "feat : dynamic import 삭제"
    This reverts commit e7701f95f833914a8f219faa82565047c0e9bfe9.

--- apps/tiebreak-web/src/pages/index.tsx
+++ apps/tiebreak-web/src/pages/index.tsx
@@ -1,10 +1,6 @@
-import dynamic from "next/dynamic";
 import React from "react";
+import Layout from "src/components/layout/Layout";
 import Home from "src/page-modules/home/Home";
-
-const Layout = dynamic(() => import("src/components/layout/Layout"), {
-  ssr: false,
-});
 
 const HomePage = () => {
   return (
