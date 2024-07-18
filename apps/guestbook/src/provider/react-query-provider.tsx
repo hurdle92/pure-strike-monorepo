6623ba91a180ebfe549aaf5b5879c5514e4b89b0
ParkSiHyungDshare <tennisflow.contact@gmail.com>
2024-07-18 18:18:03 +0900

    feat : react query default options 변경

--- apps/guestbook/src/provider/react-query-provider.tsx
+++ apps/guestbook/src/provider/react-query-provider.tsx
@@ -8,12 +8,8 @@ export default function ReactQueryProvider({ children }: PropsWithChildren) {
     new QueryClient({
       defaultOptions: {
         queries: {
-          retryOnMount: false,
-          refetchOnReconnect: false,
-          refetchIntervalInBackground: false,
-          refetchOnWindowFocus: false, // 윈도우가 다시 포커스되었을때 데이터를 refetch
-          refetchOnMount: false, // 데이터가 stale 상태이면 컴포넌트가 마운트될 때 refetch
-          retry: 0, // API 요청 실패시 재시도 하는 옵션 (설정값 만큼 재시도)
+          refetchOnWindowFocus: false,
+          refetchOnMount: false,
         },
       },
     })
