ebe1dc48c61b47c044fe9969dc8bf9beee0a8cf0
shpkc <tennisflow.contact@gmail.com>
2024-07-21 21:45:41 +0900

    feat : provider 설정 수정

--- apps/guestbook/src/provider/style-provider.tsx
+++ apps/guestbook/src/provider/style-provider.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { ReactNode, useState } from "react";
+import { useState } from "react";
 import { useServerInsertedHTML } from "next/navigation";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
 import isPropValid from "@emotion/is-prop-valid";
 import { GlobalStyle } from "src/styles/global";
 
-export default function StyleProvider({ children }: { children: ReactNode }) {
+export default function StyleProvider({ children }: { children: JSX.Element }) {
   // Only create stylesheet once with lazy initial state
   // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
