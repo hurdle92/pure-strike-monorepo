519a84b1e6864579ae7cbf9815246506de70dab4
shpkc <tennisflow.contact@gmail.com>
2024-07-21 21:46:30 +0900

    feat : Form 추가

--- apps/guestbook/src/page-modules/guestbook/GuestBook.tsx
+++ apps/guestbook/src/page-modules/guestbook/GuestBook.tsx
@@ -1,14 +1,33 @@
 "use client";
 
-import { Stack } from "pure-strike-ui";
-import { useGetGuestbook, usePostGuestbook } from "src/apis/guestbook/queries";
+import Link from "next/link";
+import { Flex, Spacer, Stack, Text } from "pure-strike-ui";
+import { useGetGuestbook } from "src/apis/guestbook/queries";
+import { BLOG_URL } from "src/constants/link";
+import { Form } from "./components/form/Form";
+import { GuestbookItem } from "./components/list/GuestbookItem";
 
 export const GuestBook = () => {
-  const {
-    data: { data, count },
-  } = useGetGuestbook();
+  const { data } = useGetGuestbook();
 
-  const { mutate, isPending } = usePostGuestbook({ content: "" });
-
-  return <Stack></Stack>;
+  return (
+    <Stack maxWidth={"760px"} margin={"0 auto"}>
+      <Spacer height={"32px"} />
+      <Flex justify={"space-between"} alignment={"center"}>
+        <Text fontSize={"32px"} fontWeight={"900"}>
+          Guest Book
+        </Text>
+        <Link href={BLOG_URL} target={"_blank"}>
+          <Text color={"blue-primary"} fontWeight={"bold"}>
+            블로그 이동
+          </Text>
+        </Link>
+      </Flex>
+      <Spacer height={"20px"} />
+      <Flex gap={"20px"} direction={"column"}>
+        {data?.data.map((item) => <GuestbookItem data={item} key={item.id} />)}
+      </Flex>
+      <Form />
+    </Stack>
+  );
 };
---
+++ apps/guestbook/src/page-modules/guestbook/components/form/Form.tsx
@@ -0,0 +1,48 @@
+import { Button, Flex, Spacer, Stack, TextField } from "pure-strike-ui";
+import { useForm } from "react-hook-form";
+import { guestbookInputDefaultValue } from "../../core/models";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { guestbookSchema } from "../../core/schema";
+import { usePostGuestbook } from "src/apis/guestbook/queries";
+import React from "react";
+
+export const Form = () => {
+  const {
+    control,
+    watch,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: "all",
+    defaultValues: guestbookInputDefaultValue,
+    resolver: yupResolver(guestbookSchema),
+  });
+
+  const { mutate, isPending } = usePostGuestbook();
+
+  const onClickSubmit = React.useCallback(() => {
+    const inputValue = watch("guestbookInput");
+    mutate({ content: inputValue });
+  }, [watch("guestbookInput")]);
+
+  return (
+    <Stack position={"fixed"} bottom={"20px"} width={"760px"}>
+      <Flex>
+        <Stack width={"100%"}>
+          <TextField
+            control={control}
+            controlKey={"guestbookInput"}
+            placeholder={"방명록을 입력해주세요."}
+          />
+        </Stack>
+        <Spacer width={"20px"} />
+        <Stack width={"100px"}>
+          <Button
+            text={"제출"}
+            disabled={!isValid || isPending}
+            onClick={onClickSubmit}
+          />
+        </Stack>
+      </Flex>
+    </Stack>
+  );
+};
