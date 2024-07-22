6e9974c1596ed65809ad98f85338445b02515252
shpkc <tennisflow.contact@gmail.com>
2024-07-23 00:28:13 +0900

    feat : geustbook item 아이콘 추가

--- apps/guestbook/src/page-modules/guestbook/components/list/GuestbookItem.tsx
+++ apps/guestbook/src/page-modules/guestbook/components/list/GuestbookItem.tsx
@@ -1,15 +1,25 @@
-import { Spacer, Stack, Text } from "pure-strike-ui";
+import { Flex, Spacer, Stack, Text } from "pure-strike-ui";
 import { GuestbookInterface } from "src/apis/guestbook/types";
 import dayjs from "dayjs";
+import { BsPersonCircle } from "react-icons/bs";
 
 export const GuestbookItem = ({ data }: { data: GuestbookInterface }) => {
   return (
     <Stack padding={"20px"} backgroundColor={"gray-200"} borderRadius={"12px"}>
-      <Text fontSize={"18px"}>{data.content}</Text>
+      <Flex alignment={"center"}>
+        <BsPersonCircle size={30} color={"gray"} />
+        <Spacer width={"8px"} />
+        <Stack>
+          <Text fontSize={"14px"} color={"gray-500"} fontWeight={"500"}>
+            익명의 방문자
+          </Text>
+          <Text fontSize={"12px"} color={"gray-500"}>
+            {dayjs(data.created_at).format("YYYY-MM-DD")}
+          </Text>
+        </Stack>
+      </Flex>
       <Spacer height={"12px"} />
-      <Text fontSize={"14px"} color={"gray-500"}>
-        Created at {dayjs(data.created_at).format("YYYY-MM-DD")}
-      </Text>
+      <Text fontSize={"18px"}>{data.content}</Text>
     </Stack>
   );
 };
