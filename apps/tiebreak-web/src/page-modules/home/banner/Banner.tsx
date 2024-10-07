a09b591e3d9fdea457d8c239467573fca0ba8469
shpkc <tennisflow.contact@gmail.com>
2024-10-08 00:41:47 +0900

    feat : Banner 반응형 추가

---
+++ apps/tiebreak-web/src/page-modules/home/banner/Banner.tsx
@@ -0,0 +1,95 @@
+import { Flex, Responsive, Stack, Text, Spacer, Button } from "pure-strike-ui";
+import { Img } from "src/components/img";
+import { ReactTyped } from "react-typed";
+
+export const Banner = () => {
+  return (
+    <Stack>
+      <Responsive
+        mobileComponent={
+          <Stack position={"relative"}>
+            <Stack position={"absolute"} width={"100%"}>
+              <Img
+                src={"/images/home/banner/banner_mobile.jpg"}
+                width={"100%"}
+                height={"100vh"}
+              />
+            </Stack>
+            <Stack position={"absolute"} width={"100%"} height={"100vh"}>
+              <Flex
+                justify={"center"}
+                alignment={"center"}
+                direction={"column"}
+              >
+                <Text
+                  fontSize={"80px"}
+                  lineHeight={"96px"}
+                  color={"white"}
+                  fontWeight={"bold"}
+                  align={"center"}
+                >
+                  테니스
+                  <br />
+                  기록을
+                  <br />
+                  즐겁게
+                </Text>
+              </Flex>
+              <Stack
+                position={"absolute"}
+                bottom={"40px"}
+                width={"calc(100% - 40px)"}
+                margin={"0 auto"}
+                left={"20px"}
+              >
+                <Button text={"지금 다운받기"} borderRadius={"14px"} />
+              </Stack>
+            </Stack>
+          </Stack>
+        }
+        desktopComponent={
+          <Stack position={"relative"}>
+            <Stack position={"absolute"} width={"100%"}>
+              <Img
+                src={"/images/home/banner/banner_desktop.jpg"}
+                width={"100%"}
+                height={"100vh"}
+              />
+            </Stack>
+            <Stack
+              position={"absolute"}
+              width={"100%"}
+              height={"100vh"}
+              backgroundColor={"black-origin-30"}
+            />
+            <Stack position={"absolute"} width={"100%"} height={"100vh"}>
+              <Flex
+                justify={"center"}
+                alignment={"center"}
+                direction={"column"}
+              >
+                <ReactTyped
+                  strings={["테니스 기록을 즐겁게"]}
+                  typeSpeed={70}
+                  backSpeed={80}
+                  startDelay={100}
+                  backDelay={2000}
+                  style={{
+                    color: "white",
+                    fontSize: "110px",
+                    fontWeight: "bold",
+                  }}
+                  loop
+                />
+                <Spacer height={"30px"} />
+                <Stack width={"160px"} height={"50px"}>
+                  <Button text={"지금 다운받기"} borderRadius={"14px"} />
+                </Stack>
+              </Flex>
+            </Stack>
+          </Stack>
+        }
+      />
+    </Stack>
+  );
+};
