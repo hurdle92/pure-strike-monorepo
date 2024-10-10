353f05ec8693163adb68a78447124a830e65b325
shpkc <tennisflow.contact@gmail.com>
2024-10-10 21:27:13 +0900

    style : Image width, height 속성 변경

--- apps/tiebreak-web/src/components/img/Img.styled.ts
+++ apps/tiebreak-web/src/components/img/Img.styled.ts
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 import { ImgStyledProps } from "./Img.types";
+import { getResponsiveStyles } from "~foundation";
 
 export const ImageContainer = styled.div<ImgStyledProps>`
-  width: ${(props) => props.width};
-  height: ${(props) => props.height};
+  ${(props) => getResponsiveStyles("width", props.width)};
+  ${(props) => getResponsiveStyles("height", props.height)};
+
   img {
     border-radius: ${(props) => props.borderRadius};
     object-fit: cover;
--- apps/tiebreak-web/src/components/img/Img.types.ts
+++ apps/tiebreak-web/src/components/img/Img.types.ts
@@ -1,12 +1,13 @@
 import { ImageProps } from "next/image";
 import { CSSProperties } from "styled-components";
+import { ResponsiveStyleProps } from "~types/componentProps";
 
 export interface ImgProps extends Pick<ImageProps, "priority"> {
   alt?: string;
   src?: string;
   isOrigin?: boolean;
-  width: string;
-  height: string;
+  width: ResponsiveStyleProps;
+  height: ResponsiveStyleProps;
   borderRadius?: string;
   style?: CSSProperties;
 }
