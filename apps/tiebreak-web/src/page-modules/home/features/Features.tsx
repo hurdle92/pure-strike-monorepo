8c155b0ab3551baf6cec5fb8f9a26bf6baddb0bd
shpkc <tennisflow.contact@gmail.com>
2024-10-14 20:13:20 +0900

    feat : Features list 완성

--- apps/tiebreak-web/src/page-modules/home/features/Features.tsx
+++ apps/tiebreak-web/src/page-modules/home/features/Features.tsx
@@ -1,11 +1,18 @@
 import { Stack, Spacer } from "pure-strike-ui";
 import { FeatureItem } from "./components/FeatureItem";
+import { FEATURES_LIST_DATA } from "./core/constants";
 
+// NOTE : 홀수만 데스크탑 reverse row
 export const Features = () => {
   return (
     <Stack>
       <Spacer height={"100px"} />
-      <FeatureItem />
+      {FEATURES_LIST_DATA.map((item, index) => {
+        const isReverse = index % 2 === 1;
+        return (
+          <FeatureItem item={item} isReverse={isReverse} key={item.title} />
+        );
+      })}
     </Stack>
   );
 };
--- apps/tiebreak-web/src/page-modules/home/features/components/FeatureItem.tsx
+++ apps/tiebreak-web/src/page-modules/home/features/components/FeatureItem.tsx
@@ -2,8 +2,15 @@ import { Flex, Spacer, Stack, Text } from "pure-strike-ui";
 import { InView } from "react-intersection-observer";
 import { Img } from "src/components/img";
 import { css } from "styled-components";
+import { FeatureItemInterface } from "../core/types";
 
-export const FeatureItem = () => {
+export const FeatureItem = ({
+  item,
+  isReverse,
+}: {
+  item: FeatureItemInterface;
+  isReverse: boolean;
+}) => {
   return (
     <InView triggerOnce={true}>
       {({ inView, ref }) => {
@@ -19,7 +26,7 @@ export const FeatureItem = () => {
             <Flex
               justify={"center"}
               alignment={["flex-start", "center"]}
-              direction={["column", "row"]}
+              direction={["column", isReverse ? "row-reverse" : "row"]}
             >
               <Stack padding={["0 16px", "0"]}>
                 <Text
@@ -27,17 +34,16 @@ export const FeatureItem = () => {
                   fontWeight={"bold"}
                   fontSize={"24px"}
                 >
-                  다이어리
+                  {item.title}
                 </Text>
                 <Spacer height={"12px"} />
                 <Text
                   fontSize={["32px", "38px"]}
                   lineHeight={"48px"}
                   fontWeight={"bold"}
+                  whitespace={"pre-wrap"}
                 >
-                  나의 테니스 플레이를
-                  <br />
-                  체계적으로 기록하고
+                  {item.description}
                 </Text>
               </Stack>
               <Spacer width={["0px", "120px"]} height={["20px", "0"]} />
@@ -48,22 +54,21 @@ export const FeatureItem = () => {
                     css={featureImgContainer}
                   >
                     <Spacer width={["40px", "0px"]} />
-                    <Img
-                      src={"/images/home/features/diary/diary_1.png"}
-                      width={["50%", "260px"]}
-                      height={["auto", "534px"]}
-                    />
-                    <Spacer width={"20px"} />
-                    <Img
-                      src={"/images/home/features/diary/diary_2.png"}
-                      width={["50%", "260px"]}
-                      height={["auto", "534px"]}
-                    />
+                    <Flex gap={["20px"]} width={"100%"}>
+                      {item.images.map((item) => (
+                        <Img
+                          key={item}
+                          src={item}
+                          width={["50%", "260px"]}
+                          height={["auto", "534px"]}
+                        />
+                      ))}
+                    </Flex>
                   </Flex>
                 </Stack>
               </Stack>
             </Flex>
-            <Spacer height={"100px"} />
+            <Spacer height={["100px", "120px"]} />
           </Stack>
         );
       }}
